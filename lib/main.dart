74dc3bc491bb4c89c6ab0fdb8f71ea1efba59076
Hassibul Hossain Shanto <hasibulhossian@gmail.com>
2026-08-23 10:11:42 +0600

    Initial commit - Task Manager Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:taskmanager/screen/onboarding/emailVerificationScreen.dart';
+import 'package:taskmanager/screen/onboarding/loginScreen.dart';
+import 'package:taskmanager/screen/onboarding/pinVerificationScreen.dart';
+import 'package:taskmanager/screen/onboarding/registrationScreen.dart';
+import 'package:taskmanager/screen/onboarding/setPasswordScreen.dart';
+import 'package:taskmanager/screen/onboarding/splashScreen.dart';
+import 'package:taskmanager/screen/task/newTaskListScreen.dart';
+import 'package:taskmanager/utility/utility.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  String? token = await ReadUserData("token");
+  if(token==null){
+    runApp(MyApp("/newTaskList"));
+  }
+  else{
+    runApp( MyApp("/login"));
+  }
+
+}
+class MyApp extends StatelessWidget{
+  final String FirstRoute;
+  MyApp(this.FirstRoute);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:"Ebo Task Manager",
+      initialRoute: FirstRoute,
+      routes: {
+        '/':(context)=> splashScreen(),
+        '/login':(context)=> loginScreen(),
+        '/registration':(context)=> registrationScreen(),
+        '/emailVerification':(context)=> emailVerificationScreen(),
+        '/pinVerification':(context)=> pinVerificationScreen(),
+        '/setPassword':(context)=> setPasswordScreen(),
+        '/newTaskList':(context)=> newTaskListScreen(),
+
+      },
+    );
+  }
+
+}
---
+++ lib/screen/profile/profileUpdateScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class profileUpdateSceen extends StatefulWidget {
+  const profileUpdateSceen({super.key});
+
+  @override
+  State<profileUpdateSceen> createState() => _profileUpdateSceenState();
+}
+
+class _profileUpdateSceenState extends State<profileUpdateSceen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
